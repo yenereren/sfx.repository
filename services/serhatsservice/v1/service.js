bd893bd708f3356f9b9a161802c508b160ec9b16
eren.yener <eren.yener@trendyol.com>
2020-06-23 11:46:37 +0300

    new servie added

---
+++ services/serhatsservice/v1/service.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+
+const service = async (req) => {
+    const user = req.user;
+    const requestUrl = "https://my-json-server.typicode.com/yenereren/sfx.fakeapi/homepage"
+    const response = (await axios.get(requestUrl)).data;
+
+    return map(response);
+}
+
+const map = (data) => {
+
+    const result = {
+        totalItems: data.totalItems,
+        totalPages: data.totalPages,
+        page: data.page,
+        size: data.size,
+    }
+
+    return result;
+}
+
+module.exports.service = service;
