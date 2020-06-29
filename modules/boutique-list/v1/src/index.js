4a8a30a7239c1529e83602b478e07daa7fb0879f
eren.yener <eren.yener@trendyol.com>
2020-06-29 10:13:22 +0300

    test

---
+++ modules/boutique-list/v1/src/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+
+function BoutiqueList() {
+  
+  return (
+    <div>
+        Boutique List
+    </div>
+  );
+}
+
+export default BoutiqueList;
