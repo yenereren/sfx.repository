8651a140cfefef8230d14070f57a2bcb097a04e8
eren.yener <eren.yener@trendyol.com>
2020-06-30 15:34:05 +0300

    v2 added

---
+++ modules/logo/v2/src/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render () {
+
+        return (
+            <div>
+                <img class="trendyol-logo-c" src="https://cdn.dsmcdn.com/web/logo/ty-elite.svg" />
+            </div>
+        )
+
+    }
+
+}
